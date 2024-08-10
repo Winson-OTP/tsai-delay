d06a717dd2530ed1d8795aecb2138e706545de75
Winson-OTP <chang.winson.055092@gmail.com>
2024-08-10 15:23:07 +0800

    更新明暗模式與遊戲機制等內容

--- script.js
+++ script.js
@@ -6,48 +6,16 @@ let buttons = []; // 按鈕html
 let ldMode = 0; // 明暗模式
 // let nineWhere = [-(board+1), -board, -(board-1), -1, 1, board-1, board, board+1]; // 九宮格取數
 let board = 9; // 棋盤大小
-
-// 隨機取地雷位置
-for (let i=0; i<board*board; i++) {
-    let num = board*board*0.14;
-    let delays = [];
-
-    while (delays.length<num) {
-        let n = randomInt(board*board);
-        if (delays.includes(n)) continue;
-        delays.push(n);
-    }
-    for (let k=0; k<board*board; k++) {
-        if (delays.includes(k)) {
-            delayList.push('1')
-        } else{
-            delayList.push('0');
-        }
-    }
-}
-
-// 統計九宮格地雷數量
-for (let i=0; i<board*board; i++) {
-    let howManyDelay = 0;
-    if (delayList[i-10] == "1" && i>9 && i%9!=0) howManyDelay++;
-    if (delayList[i-9] == "1" && i>8) howManyDelay++;
-    if (delayList[i-8] == "1" && i>7 && (i+1)%9!=0) howManyDelay++;
-    if (delayList[i-1] == "1" && i>0 && i%9!=0) howManyDelay++;
-    if (delayList[i+1] == "1" && i<81&& (i+1)%9!=0) howManyDelay++;
-    if (delayList[i+8] == "1" && i<74 && i%9!=0) howManyDelay++;
-    if (delayList[i+9] == "1" && i<73) howManyDelay++;
-    if (delayList[i+10] == "1" && i<72&& (i+1)%9!=0) howManyDelay++;
-    whatToDisplay.push(howManyDelay);
-}
+let start = false; // 開始偵測
 
 // 按鈕置入html
 for (let i=1; i<board*board+1; i++) {
     if (i%9==0) {
-        whatToPut += `<td><button id="delay${i}" oncontextmenu ="return false" style="width:30px;height:30px;"></button></td></tr>`
+        whatToPut += `<td><button id="delay${i}" class="normal" oncontextmenu ="return false" style="width:30px;height:30px;">0</button></td></tr>`
     } else if ((i-1)%9==0) {
-        whatToPut += `<tr><td><button id="delay${i}" oncontextmenu ="return false" style="width:30px;height:30px;"></button></td>`
+        whatToPut += `<tr><td><button id="delay${i}" class="normal" oncontextmenu ="return false" style="width:30px;height:30px;">0</button></td>`
     } else {
-        whatToPut += `<td><button id="delay${i}" oncontextmenu ="return false" style="width:30px;height:30px;"></button></td>`
+        whatToPut += `<td><button id="delay${i}" class="normal" oncontextmenu ="return false" style="width:30px;height:30px;">0</button></td>`
     }
 }
 whereDelayPut.innerHTML = whatToPut;
@@ -70,6 +38,43 @@ for (let i=1; i<board*board+1; i++) {
 let lord = document.getElementById('lord');
 lord.onclick = ldChange;
 
+// 開始遊戲
+function startRanbow(e) {
+    // 隨機取地雷位置
+    for (let i=0; i<board*board; i++) {
+        let num = board*board*0.14;
+        let delays = [];
+
+        while (delays.length<num) {
+            let n = randomInt(board*board);
+            if (delays.includes(n) || n==e) continue;
+            delays.push(n);
+        }
+        for (let k=0; k<board*board; k++) {
+            if (delays.includes(k)) {
+                delayList.push('1')
+            } else {
+                delayList.push('0');
+            }
+        }
+    }
+
+    // 統計九宮格地雷數量
+    for (let i=0; i<board*board; i++) {
+        let howManyDelay = 0;
+        if (delayList[i-10] == "1" && i>9 && i%9!=0) howManyDelay++;
+        if (delayList[i-9] == "1" && i>8) howManyDelay++;
+        if (delayList[i-8] == "1" && i>7 && (i+1)%9!=0) howManyDelay++;
+        if (delayList[i-1] == "1" && i>0 && i%9!=0) howManyDelay++;
+        if (delayList[i+1] == "1" && i<81&& (i+1)%9!=0) howManyDelay++;
+        if (delayList[i+8] == "1" && i<74 && i%9!=0) howManyDelay++;
+        if (delayList[i+9] == "1" && i<73) howManyDelay++;
+        if (delayList[i+10] == "1" && i<72&& (i+1)%9!=0) howManyDelay++;
+        whatToDisplay.push(howManyDelay);
+        buttons[i].innerHTML = whatToDisplay[i];
+    }
+}
+
 // 隨機取數
 function randomInt(max) {
 	return Math.floor(Math.random()*max);
@@ -77,6 +82,10 @@ function randomInt(max) {
 
 // 點按按鈕
 function click(delayNumber) {
+    if (!start) {
+        start = true;
+        startRanbow(delayNumber-1);
+    }
     let btn = buttons[delayNumber-1];
     if (btn.disabled == true) return;
     btn.disabled = true;
@@ -85,10 +94,10 @@ function click(delayNumber) {
         return gameover();
     } else {
         if (whatToDisplay[delayNumber-1] == 0) {
-            btn.style.backgroundColor = "#a9a9a9";
+            btn.setAttribute("class", "noDelayNoNine");
         } else {
             btn.innerHTML = whatToDisplay[i];
-            btn.style.backgroundColor = "#d0d0d0";
+            btn.setAttribute("class", "noDelayYesNine");
         }
         if (whatToDisplay[i] == 0) {
             let others = jentser(i);
@@ -104,10 +113,10 @@ function click(delayNumber) {
 function flag(delayNumber) {
     let btn = buttons[delayNumber-1];
     if (btn.disabled == true) return;
-    if (btn.style.backgroundColor == "rgb(205, 133, 63)") {
-        btn.style.backgroundColor = "#F0F0F0";
+    if (btn.getAttribute('class') == "yesFlag") {
+        btn.setAttribute("class", "normal");
     } else {
-        btn.style.backgroundColor = "#cd853f";
+        btn.setAttribute("class", "yesFlag");
     }
 }
 
@@ -117,12 +126,12 @@ function gameover() {
         let btn = buttons[i];
         btn.disabled = true;
         if (whatToDisplay[i] > 0 && delayList[i] == "0") {
-            btn.innerHTML = whatToDisplay[i];
-            btn.style.backgroundColor = "#d0d0d0";
+            btn.setAttribute("class", "noDelayYesNine");
         } else if (delayList[i] == "1") {
-            btn.style.backgroundColor = "#FF9797";
+            btn.setAttribute("class", "yesDelay");
+            // btn.innerHTML = "<img src=\"./delay.png\" width=25 height=25\"/>"
         } else {
-            btn.style.backgroundColor = "#a9a9a9";
+            btn.setAttribute("class", "noDelayNoNine");
         }
     }
 }
@@ -135,7 +144,7 @@ function jentser(i) {
         returnData.push(i-10+1);
     }
     if (i>9 && i%9!=0) {
-        if (delayList[i-10] == "1") return false;
+        if (delayList[i-9] == "1") return false;
         returnData.push(i-9+1);
     }
     if (i>7 && (i+1)%9!=0) {
@@ -160,6 +169,7 @@ function jentser(i) {
     }
     if (i<73) {
         if (delayList[i+9] == "1") return false;
+        returnData.push(i+9+1);
     }
     if (i<72&& (i+1)%9!=0) {
         if (delayList[i+10] == "1") return false;
